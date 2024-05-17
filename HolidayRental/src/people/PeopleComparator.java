e7e66bee267a832714729fdaa24d4ed72d5f8f35
Antoine Lalagüe--Binos <antoine.lalague-binos@etu.u-bordeaux.fr>
2024-05-17 13:01:15 +0200

    small PeopleComparator condition refactoring

--- HolidayRental/src/people/PeopleComparator.java
+++ HolidayRental/src/people/PeopleComparator.java
@@ -10,7 +10,7 @@ public class PeopleComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person p1, Person p2) {
-        if(p1 == null && p2 == null){
+        if(p1 == p2){
             return 0;
         }
         if(p1 == null) return 1;
